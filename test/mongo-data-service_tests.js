95f1f4219bff4f682ae892597d77a2ebd85b10a9
cirinoalejandro <cirinoalejandro@gmail.com>
2019-01-25 10:55:15 -0300

    Some tests

--- test/mongo-data-service_tests.js
+++ test/mongo-data-service_tests.js
@@ -1,10 +1,47 @@
 "use strict";
 
-const {MongoDataService} = require("../index");
-const {expect} = require("chai");
-
 describe("Mongo Data Service", () => {
+  const {MongoDataService} = require("../index");
+  const {expect} = require("chai");
+
   it("should be called MongoDataService", () => {
     expect(MongoDataService.name).to.equal("MongoDataService");
+  });
+
+  describe("constructor", () => {
+    function sut() {
+      return new MongoDataService(dependencies);
+    }
+
+    let dependencies = null;
+    beforeEach(() => {
+      dependencies = {
+        dao: {for(){}},
+        config: {}
+      };
+    });
+
+    it("should fail if dao is not in dependencies", () => {
+      Reflect.deleteProperty(dependencies, "dao");
+      expect(sut).to.throw("dao is mandatory for MongoDataService");
+    });
+  });
+
+  describe("#getById()", () => {
+  });
+
+  describe("#delete()", () => {
+  });
+
+  describe("#getExisting()", () => {
+  });
+
+  describe("#update()", () => {
+  });
+
+  describe("#getCountedList()", () => {
+  });
+
+  describe("#getAggregateCount()", () => {
   });
 });
